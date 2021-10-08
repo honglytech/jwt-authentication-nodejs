474b068653aeda26c91e8e28269e08ae47ad25d0
lyhd <lyhong.rupp@gmail.com>
2021-10-08 14:08:51 +1100

    generate refresh token & deauthenticate refresh token (log out)

--- routes/auth.js
+++ routes/auth.js
@@ -117,7 +117,7 @@ router.post("/login", async (req, res) => {
     });
   }
 
-  // Send JWT
+  // Send JWT access token
   const accessToken = await JWT.sign(
     { email },
     process.env.ACCESS_TOKEN_SECRET,
@@ -126,9 +126,83 @@ router.post("/login", async (req, res) => {
     }
   );
 
+  // Refresh token
+  const refreshToken = await JWT.sign(
+    { email },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: "1m",
+    }
+  );
+
+  // Set refersh token in refreshTokens array
+  refreshTokens.push(refreshToken);
+
   res.json({
     accessToken,
+    refreshToken,
   });
+});
+
+let refreshTokens = [];
+
+// Create new access token from refresh token
+router.post("/token", async (req, res) => {
+  const refreshToken = req.header("x-auth-token");
+
+  // If token is not provided, send error message
+  if (!refreshToken) {
+    res.status(401).json({
+      errors: [
+        {
+          msg: "Token not found",
+        },
+      ],
+    });
+  }
+
+  // If token does not exist, send error message
+  if (!refreshTokens.includes(refreshToken)) {
+    res.status(403).json({
+      errors: [
+        {
+          msg: "Invalid refresh token",
+        },
+      ],
+    });
+  }
+
+  try {
+    const user = await JWT.verify(
+      refreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    );
+    // user = { email: 'jame@gmail.com', iat: 1633586290, exp: 1633586350 }
+    const { email } = user;
+    const accessToken = await JWT.sign(
+      { email },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "10s" }
+    );
+    res.json({ accessToken });
+  } catch (error) {
+    res.status(403).json({
+      errors: [
+        {
+          msg: "Invalid token",
+        },
+      ],
+    });
+  }
+});
+
+// Deauthenticate - log out
+// Delete refresh token
+router.delete("/logout", (req, res) => {
+  const refreshToken = req.header("x-auth-token");
+
+  refreshTokens = refreshTokens.filter((token) => token !== refreshToken);
+  res.sendStatus(204);
 });
 
 module.exports = router;
